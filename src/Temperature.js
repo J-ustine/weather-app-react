065941bfd4d9780dc18df76581f30729f1ac4d6f
J-ustine <78701219+J-ustine@users.noreply.github.com>
2021-03-20 12:30:41 +0100

    Added Degree conversion between Celsius to Fahrenheit

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Temperature(props) {
-  return (
-    <div className="col-4">
-      <div className="temperature">
-        <span id="number">{Math.round(props.data.temperature)}</span>
-        <button id="celsius" className="active unit">
-          °C
-        </button>
-        <span className="unit"> |</span>
-        <button id="farenheit" className="unit">
-          °F
-        </button>
+  const [unit, setUnit] = useState("celcius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="col-4">
+        <div className="temperature">
+          <span id="number">{Math.round(props.data.temperature)}</span>
+          <button id="celsius" className="unit">
+            °C
+          </button>
+          <span className="unit"> |</span>
+          <button
+            id="fahrenheit"
+            className="active unit"
+            onClick={showFahrenheit}
+          >
+            °F
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let fahrenheit = (props.data.temperature * 9) / 5 + 32;
+    return (
+      <div className="col-4">
+        <div className="temperature">
+          <span id="number">{Math.round(fahrenheit)}</span>
+          <button id="celsius" className="active unit" onClick={showCelsius}>
+            °C
+          </button>
+          <span className="unit"> |</span>
+          <button id="fahrenheit" className=" unit">
+            °F
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
